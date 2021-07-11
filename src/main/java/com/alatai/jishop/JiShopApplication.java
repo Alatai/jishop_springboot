0837cd161ed162a474ec90dd2130b1819d92d024
qipeng <qp19940413@163.com>
2021-07-11 18:10:30 +0900

    Initial commit

---
+++ src/main/java/com/alatai/jishop/JiShopApplication.java
@@ -0,0 +1,13 @@
+package com.alatai.jishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alatai/jishop/JiShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alatai.jishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
