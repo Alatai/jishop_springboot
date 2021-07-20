b351bd57d582b5facf0f3df786bf78cedb064850
qipeng <qp19940413@163.com>
2021-07-20 10:40:54 +0900

    fix bug

--- src/main/java/com/alatai/jishop/dao/ProductDao.java
+++ src/main/java/com/alatai/jishop/dao/ProductDao.java
@@ -2,7 +2,6 @@ package com.alatai.jishop.dao;
 
 import com.alatai.jishop.entity.Category;
 import com.alatai.jishop.entity.Product;
-import com.alatai.jishop.entity.Property;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
